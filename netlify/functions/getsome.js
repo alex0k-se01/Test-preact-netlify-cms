41cd996be917ffc85d1621ce31aa436e77d58efd
alex0k-se1 <alex0k.se1@gmail.com>
2022-07-19 00:11:51 -0700

    ...

--- netlify/functions/getsome.js
+++ netlify/functions/getsome.js
@@ -1,8 +1,13 @@
+const fetch = require('node-fetch');
+
+const API_ENDPOINT = 'https://reactnative.dev/movies.json';
+
 exports.handler = async function (event, context) {
   const { identity, user } = context.clientContext;
-  const username = "%Username%";
+  const username = user != null ? user.user_metadata.full_name : "%Username%";
+  const data = await (await fetch(API_ENDPOINT)).json();
   return {
     statusCode: 200,
-    body: JSON.stringify({ message: "Hello World, " + username + "!", identity, user }),
+    body: JSON.stringify({ message: `Hello World, ${username}!`, identity, user, data }),
   };
 }
